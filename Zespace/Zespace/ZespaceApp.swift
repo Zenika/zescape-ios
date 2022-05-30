49db84dd7f0c5a54507826149c0ec5c412ade952
Anthony MARQUET <anthony.marquet@zenika.com>
2022-05-30 15:39:43 +0200

    :tada: First commit
    Add iOS project

---
+++ Zespace/Zespace/ZespaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZespaceApp.swift
+//  Zespace
+//
+//  Created by Anthony on 30/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct ZespaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
