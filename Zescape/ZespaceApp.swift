f872a706ab76334af9f9a82c6f973b049299f0a4
Anthony MARQUET <anthony.marquet@zenika.com>
2023-09-20 09:15:39 +0200

    feat: init sdk, add associated domain, url type

--- Zescape/ZespaceApp.swift
+++ Zescape/ZespaceApp.swift
@@ -7,11 +7,23 @@
 
 import SwiftUI
 import FirebaseCore
+import BranchSDK
+
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     FirebaseApp.configure()
+      
+    Branch.getInstance().initSession(launchOptions: launchOptions) { (params, error) in
+      print(params as? [String: AnyObject] ?? {})
+      // Access and use deep link data here (nav to page, display content, etc.)
+    }
+      
+    Branch.setUseTestBranchKey(true)
+      
+    Branch.getInstance().validateSDKIntegration()
+
 
     return true
   }
@@ -22,7 +34,11 @@ struct ZespaceApp: App {
     var body: some Scene {
         WindowGroup {
            // ContentView(rGuess: 0.5, gGuess: 0.5, bGuess: 0.5)
-            SplashView()
+            SplashView().onOpenURL(perform: { url in
+                Branch.getInstance().handleDeepLink(url)
+        })
+           
+                
         }
     }
 }
